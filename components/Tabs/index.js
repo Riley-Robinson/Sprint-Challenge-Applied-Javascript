3f3da8fc6d1b5f7fb74389eef9e7e9b84a17ab32
Riley-Robinson <rrtoinfinity@gmail.com>
2020-03-13 13:15:39 -0400

    "herder,tabs done card 50%"

--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -7,3 +7,17 @@
 //
 //  The tab component should look like this:
 //    <div class="tab">topic here</div>
+
+
+topics = [];
+    axios
+        .get("https://lambda-times-backend.herokuapp.com/topics")
+        .then(response => {
+            response.data.topics.forEach(element => {
+                let stuffDiv = document.createElement("div");
+                stuffDiv.classList.add("tab");
+                stuffDiv.textContent = `${element}`;
+                console.log(stuffDiv);
+                document.querySelector(".topics").append(stuffDiv);
+            });
+        });
